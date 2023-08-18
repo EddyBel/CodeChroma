28fc7defbe91910ab27d04a61953a1fc49d7f83e
EddyBel <dante61918@gmail.com>
2023-08-17 21:50:51 -0600

    feat: Updated version program

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("./README.md", "r")
 setup(
     name="CodeChroma",
     packages=["CodeChroma"],
-    version=1.2,
+    version="1.5.0",
     description="This project is a simple library to color text in the terminal with python.",
     long_description=readme.read(),
     long_description_content_type="text/markdown",
@@ -14,14 +14,14 @@ setup(
     url="https://github.com/EddyBel/CodeChroma",
     download_url="https://github.com/EddyBel/CodeChroma",
     keywords=["colors", "syntax", "terminal", "markdown"],
-    classifiers=[ ],
+    classifiers=[],
     license="MIT",
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=[  
+    install_requires=[
         "colorama==0.4.6",
-        "colored==1.4.4",
-        "Pygments==2.14.0",
-        "termcolor==2.2.0",
+        "colored==2.2.3",
+        "Pygments==2.16.1",
+        "termcolor==2.3.0",
     ],
 )
